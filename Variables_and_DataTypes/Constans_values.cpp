bded32fba85c20dad891e58c1d6254a8e5cccbe3
Darshansalunke53 <darshansalunke53@gmail.com>
2023-03-07 01:28:22 +0530

    constant vaules

---
+++ Variables_and_DataTypes/Constans_values.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+
+{
+    int a=10;
+    int b=20;
+
+    cout<<"Sum of a and b is:"<<a+b<<endl;
+    cout<<"Muliply a with number:"<<a*100<<endl;
+    cout<<"Division of number:"<<100/10;         //constant values
+    return 0;
+}
