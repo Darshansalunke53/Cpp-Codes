6f6f3cb0a5fb25c6475ed90007acc12de634bbb2
Darshansalunke53 <darshansalunke53@gmail.com>
2023-03-07 01:29:41 +0530

    Gloable and Local variable

---
+++ Variables_and_DataTypes/Gloabal_and_Local_Varables.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int num=100;  //Gloable variable
+
+int main()
+
+{
+    int num=20;  //Loacal variable
+    cout<<"Value of num is:"<<num;
+    return 0;
+}
