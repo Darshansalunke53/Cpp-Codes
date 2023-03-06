3a1f0c6197b28fc6d321671b298ff4c6f63a791d
Darshansalunke53 <darshansalunke53@gmail.com>
2023-03-07 01:29:05 +0530

    constant variable

---
+++ Variables_and_DataTypes/Constant_variables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+#define pi 3.141592
+
+int main()
+
+{
+    const int num=30;
+    cout<<num<<"\n";
+    cout<<pi<<"\n";
+    return 0;
+}
