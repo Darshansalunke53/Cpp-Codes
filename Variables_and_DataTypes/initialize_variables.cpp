859121b34627bdabfedc29e3ff7edf8fe9d43c5d
Darshansalunke53 <darshansalunke53@gmail.com>
2023-03-07 01:30:20 +0530

    Variable initialization

---
+++ Variables_and_DataTypes/initialize_variables.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+
+{
+    //variable_type variable_name-syntax
+
+    int num;
+    cout<<"Number is:"<<num;
+    int num1{0};
+    cout<<"\nNumber is:"<<num1;
+    int num2=10;
+    cout<<"\nNumber is:"<<num2;
+    int num3(34);
+    cout<<"\nNumber is:"<<num3;
+    return 0;
+}
