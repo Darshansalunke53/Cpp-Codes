ad4af90ead6287c45431ebfeac6625e5be96a19d
Darshansalunke53 <darshansalunke53@gmail.com>
2023-03-09 23:25:54 +0530

    Assignment Operator

---
+++ Statement-and-Operators/Assignmentop.cpp
@@ -0,0 +1,20 @@
+//Assignment Operator(=)
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+
+{
+    int num1{10};
+    int num2{20};
+    cout<<"Value of num1 and num2 is:"<<num1<<" "<<num2<<endl;
+
+    num1=100;
+
+    cout<<"Value of num1 is:"<<num1;
+    
+    return 0;
+}
